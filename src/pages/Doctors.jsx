cd8594f1d9081c69ad50e6b485ac3cd6c255aed8
Shakil Alam <itxshakil@gmail.com>
2024-10-09 13:32:48 +0530

    Feat- Add footer and some other changes

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Doctors = () => {
+  const { speciality } = useParams()
+  console.log(speciality)
+
   return (
     <div>
-      
+       
     </div>
   )
 }
