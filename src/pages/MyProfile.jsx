c4882a6987406474742840fc433bff5ef004f1c3
itxnargis <itxnargiskhatun@gmail.com>
2024-10-23 14:42:56 +0530

    fix admin login errors

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import assets from "../assets/assets"
+import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 
