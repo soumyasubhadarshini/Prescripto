d6d1007fcba19520a95442cd0374a8210aa5bef7
soumyasubhadarshini <soumyasubhadarshini0@gmail.com>
2025-02-23 21:33:28 +0530

    backend updates

---
+++ prescripto-backend/routes/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoutes.js'
+import doctorRouter from './routes/doctorRoutes.js'
+import userRouter from './routes/userRoute.js'
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/admin', adminRouter)
+app.use('/api/doctor', doctorRouter)
+app.use('/api/user', userRouter)
+
+app.get('/', (req, res) => {
+      res.send('API WORKING')
+})
+
+app.listen(port, () => console.log("Server Started", port))
